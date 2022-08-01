22368b61e7fc337fbdebb419a8f55c34b096c8b0
Dagbrain <dagrainion@gmail.com>
2022-08-01 22:23:42 +0100

    changed  cons js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,22 @@
 const contractAddress = "0xee1FBb34fFd9daFF33AE0a6979B8AdF30B0feea4"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to Dagrainion Warriors NFT Mint!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
+const welcome_p = 'Owning a Dagrainion Warrior NFT is your proof of fractionalised ownership of our vast investment plays. Each NFT returns the same % of rewards back to the hodlers.';
+const h1_presale_coming_soon = "NFT Warriors are on the ship and will be on planet POLYGON soon!!";
+const h1_presale_mint = "Warrior Listed Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const h2_presale_coming_soon = "Warrior Listed Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
-const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const p_presale_coming_soon = "Warrior Listed pre-sale coming soon. Join our Discord for official updates";
+const p_presale_mint_not_whitelisted = "You are not Warrior Listed for the pre-sale.. 😢";
+const p_presale_mint_whitelisted = "You're on the Warrior List for the pre-sale! 🎉";
+const p_presale_mint_already_minted = "You've already claimed your Warrior List mint. Thank you! 🎉";
+const p_public_mint = "No Warrior List needed. Public minting is now open! 🎉";
+const button_presale_coming_soon = "Get on the Warrior-List";
+const button_presale_mint_whitelisted = "Mint Your Warrior NFT";
+const button_presale_mint_not_whitelisted = "Get on the Warrior List";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_public_mint = "Mint Your Warrior NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
